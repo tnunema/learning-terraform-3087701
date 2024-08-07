3fd5b4a934722dd9911271424c9bee6b00dd97da
Soul_S3v3n <tommy.nunemann19973@gmail.com>
2024-08-07 09:08:14 -0400

    use module for security group

--- main.tf
+++ main.tf
@@ -29,6 +29,20 @@ vpc_security_group_ids = [aws_security_group.blog.id]
   }
 }
 
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.2"
+  name    = blog_new
+
+  vpc_id = data.aws_vpc.default.id
+
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = [0.0.0.0/0]
+
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = [0.0.0.0/0]
+}
+
 resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow http and https in, allow everything out"
@@ -43,7 +57,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group.blog.id
+  security_group_id = module.blog_sg.security_group_id
 }
 
 resource "aws_security_group_rule" "blog_https_in" {
