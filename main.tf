51d02ece722af38ec35dcdd7907ec9cdffd4b913
Soul_S3v3n <tommy.nunemann19973@gmail.com>
2024-08-08 10:26:13 -0400

    load balancer

--- main.tf
+++ main.tf
@@ -70,10 +70,10 @@ module "alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      targets = {
+      targets {
         my_target {
-        target_id        = "aws_instance.blog.id"
-        port  = 80 
+        target_id  = "aws_instance.blog.id"
+        port       = 80 
         }
     }
    }
