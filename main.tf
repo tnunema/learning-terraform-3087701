ab7c7ae2c30582f2104e2d2a0a06b78d190fd706
Soul_S3v3n <tommy.nunemann19973@gmail.com>
2024-08-08 10:24:42 -0400

    load balancer

--- main.tf
+++ main.tf
@@ -50,32 +50,39 @@ module "alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name              = "blog-alb"
-  vpc_id            = "module.blog_vpc.vpc.id"
-  subnets           = module.blog_vpc.public_subnets
-  security_groups   = module.blog_sg.security_group_id
-  }
 
-  http_tcp_listeners = {
+  vpc_id            = module.vlog_vpc.vpc.id
+  subnets           = module.blog_vpc.public_subnets
+  security_group_id = module.blog_sg.security_group_id
+
+  listeners = {
+    http_tcp_listeners = {
       port               = 80
       protocol           = "HTTP"
-      target_group_index = 0  
+      target_group_index = 0 
+    }
+
     }
 
   target_groups = {
-    ex-instance = {
+    {
       name_prefix      = "blog-"
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = "aws_instance.blog.id"
+      targets = {
+        my_target {
+        target_id        = "aws_instance.blog.id"
+        port  = 80 
+        }
     }
+   }
   }
 
   tags = {
-    Environment = "Development"
+    Environment = "Dev"
   }
 }
-
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.2"
